7512858db657d5b908801b385cf6c09369336947
Meujks <maaxkarlson@live.se>
2018-10-18 16:33:32 +0200

    Created a new db each time file is read and created a product table

--- Database/db.sql
+++ Database/db.sql
@@ -1,1 +1,17 @@
-CREATE DATABASE IF NOT EXISTS 'ShopDB';
+DROP DATABASE IF EXISTS `ShopDB`;
+CREATE DATABASE `ShopDB`;
+USE `ShopDB`;
+
+-- **********************************************
+-- *                                            *
+-- *                  Tables                    *
+-- *                                            *
+-- **********************************************
+
+CREATE TABLE `Products` (
+  `pid` INT NOT NULL UNIQUE AUTO_INCREMENT,
+  `pName` VARCHAR(256) NOT NULL UNIQUE,
+  `pCost` INT NOT NULL,
+  `pQuantity` INT NOT NULL,
+  PRIMARY KEY (`pid`)
+);
