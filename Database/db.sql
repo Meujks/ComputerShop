a203335382b9b4c0a33ab8a3a6ec74c082d1714d
Meujks <maaxkarlson@live.se>
2018-10-18 16:41:40 +0200

    Inserting Desktop and adding type

--- Database/db.sql
+++ Database/db.sql
@@ -11,7 +11,17 @@ USE `ShopDB`;
 CREATE TABLE `Products` (
   `pid` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `pName` VARCHAR(256) NOT NULL UNIQUE,
+  `pType` VARCHAR(256) NOT NULL UNIQUE,
   `pCost` INT NOT NULL,
   `pQuantity` INT NOT NULL,
   PRIMARY KEY (`pid`)
 );
+
+-- **********************************************
+-- *                                            *
+-- *                 Values                     *
+-- *                                            *
+-- **********************************************
+
+ INSERT INTO `Products`(`pName`,`pType`, `pCost`, `pQuantity`)
+ VALUE("Desktop","Extreme", 550, 100);
