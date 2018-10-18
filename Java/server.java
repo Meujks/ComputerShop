5895990c787acbb72ecabd23ecd57b3c4d19d37c
Meujks <maaxkarlson@live.se>
2018-10-18 17:27:54 +0200

    Trying out layout for server

--- Java/server.java
+++ Java/server.java
@@ -1,23 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Server {
-
-	public static void main(String[] args)
+	
+	public static void getAllProducts(Connection conn)
 	{
-		try
-		{
-			// Create connection to the associated Database located at localhost 
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ShopDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
-			
+		try{
 			// Create a statement for query
 			Statement myStmt = conn.createStatement();
 			
 			// add result from query to result variable
 			ResultSet result = myStmt.executeQuery("select * from Products");
-
+	
 			// print out the name from table
 			while(result.next())
 			{
@@ -28,6 +25,18 @@ public class Server {
 			   exc.printStackTrace();
 
 		}
+	}
+	public static void main(String[] args)
+	{
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ShopDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+			getAllProducts(conn);
+	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
 	}
 
 }
